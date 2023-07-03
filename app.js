9adb29386a15056ab92d198240a3e94fb7385e8a
edm.nd <129580277+Tsuuukiii@users.noreply.github.com>
2023-07-03 20:45:06 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -6,4 +6,47 @@ hamburger.onclick = function(){
     navbar = document.querySelector(".navbar");
     navbar.classList.toggle("active");
 }
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+function on1() {
+  document.getElementById("overlay1").style.display = "block";
+}
 
+function off1() {
+  document.getElementById("overlay1").style.display = "none";
+}
+function on2() {
+  document.getElementById("overlay2").style.display = "block";
+}
+
+function off2() {
+  document.getElementById("overlay2").style.display = "none";
+}
+function enlargeImage(image) {
+    var enlargedImage = document.createElement('img');
+    enlargedImage.src = image.src;
+    enlargedImage.classList.add('enlarged-image');
+    enlargedImage.onclick = function() {
+      document.body.removeChild(this);
+    };
+    document.body.appendChild(enlargedImage);
+  }
+  let mybutton = document.getElementById("myBtn");
+
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  function topFunction() {
+
+    document.documentElement.scrollTop = 0;}
